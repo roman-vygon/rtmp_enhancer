e55908696412fae8f8f8dc14521439dad3c8a2de
roman-vygon <roman.vygon@gmail.com>
2020-06-19 11:28:30 +0700

    Add restream pipeline
    Restreaming now supports enhancing via neural network, fps still low, missing frames are replaced with last received frame

--- receiver.py
+++ receiver.py
@@ -1,11 +1,13 @@
 import os
 import queue
 import threading
-
+import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 from tensorflow import keras
 from twitchstream.outputvideo import TwitchBufferedOutputStream
+
+from stream import Streamer
 
 output_dir = 'frames'
 q = queue.Queue()
@@ -17,61 +19,38 @@ model = keras.models.Model(inputs, output)
 
 stream_addr = 'rtmp://192.168.1.5/live/test'
 cap = cv2.VideoCapture(stream_addr)
-print(cap.isOpened())
 
-width = 2048
-height = 1152
+width = 128 * 4
+height = 128 * 4
 fps = 30.
 
-last_frame = np.zeros((height, width, 3))
-
-
-def twitch_stream():
-    videostream = TwitchBufferedOutputStream(
-        twitch_stream_key='live_151094258_mOH5qXYKHsj6PqrTazKFAiCboLUnKn',
-        width=width,
-        height=height,
-        fps=fps,
-        enable_audio=False,
-        verbose=False)
-    while True:
-        if videostream.get_video_frame_buffer_state() < 30:
-            videostream.send_video_frame(last_frame)
+stream = Streamer(height, width, fps)
 
 
 def background():
     while True:
         ret, frame = cap.read()
         if frame is not None:
-            # cv2.imshow('frame', frame)
+            #im_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             q.put(frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
     cap.release()
-    cv2.destroyAllWindows()
 
 
 def foreground():
-    num = 0
     while True:
         frame = q.get()
         frame = frame / 255.0
-
         sr = model.predict(np.expand_dims(frame, axis=0))[0]
 
-        sr = ((sr + 1) / 2.) * 255
+        sr = (sr + 1) / 2.
 
-        # Save the results:
-        # cv2.imwrite(os.path.join(output_dir, str(num) + '.png'), sr)
-        last_frame = sr
-        num += 1
+        #if stream.get_video_frame_buffer_state() < fps:
+        stream.send_video_frame(sr, frame_counter=None)
         q.task_done()
 
 
 b = threading.Thread(name='background', target=background)
 f = threading.Thread(name='foreground', target=foreground)
-t = threading.Thread(name='twitch', target=twitch_stream)
 
 b.start()
 f.start()
-t.start()
--- stream.py
+++ stream.py
@@ -1,73 +1,71 @@
-# This script copies the video frame by frame
 import cv2
 import subprocess as sp
 import time
 import numpy as np
 import queue
 import threading
-twitch_stream_key = 'live_151094258_mOH5qXYKHsj6PqrTazKFAiCboLUnKn'
-
-command = []
-command.extend([
-    'FFMPEG',
-    '-loglevel', 'verbose',
-    '-y',  # overwrite previous file/stream
-    '-analyzeduration', '1',
-    '-f', 'rawvideo',
-    '-r', '%d' % 30.,  # set a fixed frame rate
-    '-vcodec', 'rawvideo',
-    # size of one frame
-    '-s', '%dx%d' % (640, 480),
-    '-pix_fmt', 'rgb24',  # The input are raw bytes
-    '-thread_queue_size', '1024',
-    '-i', '-',  # The input comes from a pipe
-
-])
-command.extend([
-    '-ar', '8000',
-    '-ac', '1',
-    '-f', 's16le',
-    '-i', 'http://www.hochmuth.com/mp3/Tchaikovsky_Nocturne__orch.mp3',
-])
-command.extend([
-    # VIDEO CODEC PARAMETERS
-    '-vcodec', 'libx264',
-    '-r', '%d' % 30.,
-    '-b:v', '3000k',
-    '-s', '%dx%d' % (640, 480),
-    '-preset', 'faster', '-tune', 'zerolatency',
-    '-crf', '23',
-    '-pix_fmt', 'yuv420p',
-
-    '-minrate', '3000k', '-maxrate', '3000k',
-    '-bufsize', '12000k',
-    '-g', '60',  # key frame distance
-    '-keyint_min', '1',
-
-    # AUDIO CODEC PARAMETERS
-    '-acodec', 'libmp3lame', '-ar', '44100', '-b:a', '160k',
-    # '-bufsize', '8192k',
-    '-ac', '1',
-    '-map', '0:v', '-map', '1:a',
-
-    '-threads', '2',
-    # STREAM TO TWITCH
-    '-f', 'flv', 'rtmp://live-hel.twitch.tv/app/%s' %
-          twitch_stream_key
-])
 
 
 class Streamer:
     def __init__(self, height, width, fps):
         self.ffmpeg = 'FFMPEG'
         self.dimension = '{}x{}'.format(width, height)
-        self.f_format = 'bgr24'
+        self.twitch_stream_key = 'live_151094258_mOH5qXYKHsj6PqrTazKFAiCboLUnKn'
         self.fps = fps
 
         self.height = height
         self.width = width
+        command = []
+        command.extend([
+            'FFMPEG',
+            '-loglevel', 'verbose',
+            '-y',  # overwrite previous file/stream
+            '-analyzeduration', '1',
+            '-f', 'rawvideo',
+            '-r', '%d' % self.fps,  # set a fixed frame rate
+            '-vcodec', 'rawvideo',
+            # size of one frame
+            '-s', '%dx%d' % (self.width, self.height),
+            '-pix_fmt', 'bgr24',  # The input are raw bytes
+            '-thread_queue_size', '1024',
+            '-i', '-',  # The input comes from a pipe
 
-        self.last_frame = np.ones((self.height, self.width, 3))
+        ])
+        command.extend([
+            '-ar', '8000',
+            '-ac', '1',
+            '-f', 's16le',
+            '-i', 'http://www.hochmuth.com/mp3/Tchaikovsky_Nocturne__orch.mp3',
+        ])
+        command.extend([
+            # VIDEO CODEC PARAMETERS
+            '-vcodec', 'libx264',
+            '-r', '%d' % self.fps,
+            '-b:v', '3000k',
+            '-s', '%dx%d' % (self.width, self.height),
+            '-preset', 'faster', '-tune', 'zerolatency',
+            '-crf', '23',
+            '-pix_fmt', 'yuv420p',
+            #'-vf', 'negate',
+
+            '-minrate', '3000k', '-maxrate', '3000k',
+            '-bufsize', '12000k',
+            '-g', '60',  # key frame distance
+            '-keyint_min', '1',
+
+            # AUDIO CODEC PARAMETERS
+            '-acodec', 'libmp3lame', '-ar', '44100', '-b:a', '160k',
+            # '-bufsize', '8192k',
+            '-ac', '1',
+            '-map', '0:v', '-map', '1:a',
+
+            '-threads', '2',
+            # STREAM TO TWITCH
+            '-f', 'flv', 'rtmp://live-hel.twitch.tv/app/%s' %
+                  self.twitch_stream_key
+        ])
+
+        self.last_frame = np.random.random((self.height, self.width, 3))
 
         self.last_frame_time = None
         self.next_video_send_time = None
@@ -90,7 +88,7 @@ class Streamer:
         """
         return self.q_video.qsize()
 
-    def send_video_frame(self, frame):
+    def do_send_video_frame(self, frame):
         """Send frame of shape (height, width, 3)
         with values between 0 and 1.
         Raises an OSError when the stream is closed.
@@ -108,6 +106,23 @@ class Streamer:
             # downstream
             raise
 
+    def send_video_frame(self, frame, frame_counter=None):
+        """send frame of shape (height, width, 3)
+        with values between 0 and 1
+        :param frame: array containing the frame.
+        :type frame: numpy array with shape (height, width, 3)
+            containing values between 0.0 and 1.0
+        :param frame_counter: frame position number within stream.
+            Provide this when multi-threading to make sure frames don't
+            switch position
+        :type frame_counter: int
+        """
+        if frame_counter is None:
+            frame_counter = self.frame_counter
+            self.frame_counter += 1
+
+        self.q_video.put((frame_counter, frame))
+
     def _send_video_frame(self):
         start_time = time.time()
         try:
@@ -123,7 +138,7 @@ class Streamer:
             self.last_frame = frame
 
         try:
-            send_video_frame(frame)
+            self.do_send_video_frame(frame)
         except OSError:
             # stream has been closed.
             # This function is still called once when that happens.
@@ -154,9 +169,103 @@ class Streamer:
         self.proc.wait()
 
 
-stream = Streamer(480, 640, 30)
+class RepeatStreamer:
+    def __init__(self, height, width, fps):
+        self.ffmpeg = 'FFMPEG'
+        self.dimension = '{}x{}'.format(width, height)
+        self.twitch_stream_key = 'live_151094258_mOH5qXYKHsj6PqrTazKFAiCboLUnKn'
+        self.fps = fps
 
-while True:
-    if stream.get_video_frame_buffer_state() < 30:
-        frame = np.random.rand(480, 640, 3)
-        stream.send_video_frame(frame)
+        self.height = height
+        self.width = width
+        command = []
+        command.extend([
+            'FFMPEG',
+            '-loglevel', 'verbose',
+            '-y',  # overwrite previous file/stream
+            '-analyzeduration', '1',
+            '-f', 'rawvideo',
+            '-r', '%d' % self.fps,  # set a fixed frame rate
+            '-vcodec', 'rawvideo',
+            # size of one frame
+            '-s', '%dx%d' % (self.width, self.height),
+            '-pix_fmt', 'bgr24',  # The input are raw bytes
+            '-thread_queue_size', '1024',
+            '-i', '-',  # The input comes from a pipe
+
+        ])
+        command.extend([
+            '-ar', '8000',
+            '-ac', '1',
+            '-f', 's16le',
+            '-i', 'http://www.hochmuth.com/mp3/Tchaikovsky_Nocturne__orch.mp3',
+        ])
+        command.extend([
+            # VIDEO CODEC PARAMETERS
+            '-vcodec', 'libx264',
+            '-r', '%d' % self.fps,
+            '-b:v', '3000k',
+            '-s', '%dx%d' % (self.width, self.height),
+            '-preset', 'faster', '-tune', 'zerolatency',
+            '-crf', '23',
+            '-pix_fmt', 'yuv420p',
+            '-vf', 'negate',
+
+            '-minrate', '3000k', '-maxrate', '3000k',
+            '-bufsize', '12000k',
+            '-g', '60',  # key frame distance
+            '-keyint_min', '1',
+
+            # AUDIO CODEC PARAMETERS
+            '-acodec', 'libmp3lame', '-ar', '44100', '-b:a', '160k',
+            # '-bufsize', '8192k',
+            '-ac', '1',
+            '-map', '0:v', '-map', '1:a',
+
+            '-threads', '2',
+            # STREAM TO TWITCH
+            '-f', 'flv', 'rtmp://live-hel.twitch.tv/app/%s' %
+                  self.twitch_stream_key
+        ])
+
+        self.last_frame = np.ones((self.height, self.width, 3))
+
+        self.last_frame_time = None
+        self.next_video_send_time = None
+        self.frame_counter = 0
+        self.q_video = queue.PriorityQueue()
+
+        self.proc = sp.Popen(command, stdin=sp.PIPE)
+
+        # self.t = threading.Timer(0.0, self._send_video_frame)
+        # self.t.daemon = True
+        # self.t.start()
+
+        self.lastframe = np.ones((self.height, self.width, 3))
+        self._send_last_video_frame()  # Start sending the stream
+
+    def _send_last_video_frame(self):
+        try:
+            self.send_video_frame(self.lastframe)
+        except OSError:
+            # stream has been closed.
+            # This function is still called once when that happens.
+            pass
+        else:
+            # send the next frame at the appropriate time
+            threading.Timer(1. / self.fps,
+                            self._send_last_video_frame).start()
+
+    def send_video_frame(self, frame):
+        """Send frame of shape (height, width, 3)
+        with values between 0 and 1.
+        :param frame: array containing the frame.
+        :type frame: numpy array with shape (height, width, 3)
+            containing values between 0.0 and 1.0
+        """
+        self.lastframe = frame
+
+    def __exit__(self):
+        self.proc.stdin.close()
+        self.proc.stderr.close()
+        self.proc.wait()
